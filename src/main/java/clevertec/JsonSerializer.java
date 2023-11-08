fefccc6ce88b5172f21f1866d38dec8a93392232
EnoticSho <paradise666hell@mail.ru>
2023-11-08 04:25:44 +0300

    feat: added JsonSerializer

---
+++ src/main/java/clevertec/JsonSerializer.java
@@ -0,0 +1,12 @@
+package clevertec;
+
+public class JsonSerializer {
+
+    public String serialize(Object object) {
+
+    }
+
+    public <T> T deserialize(String json, Class<T> clazz) {
+
+    }
+}
